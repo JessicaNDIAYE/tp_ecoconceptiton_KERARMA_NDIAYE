87f32c4b637e103a92ced8640c2c458a34883a98
jess_nd <jessica.ndiaye@etu.univ-lyon1.fr>
2026-02-10 00:41:07 +0100

    TP API

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from .database import Base
+
+class Pokemon(Base):
+    __tablename__ = "pokemon"
+
+    id = Column(Integer, primary_key=True, index=True)
+    identifier = Column(String(100), nullable=False)
+    species_id = Column(Integer)
+    height = Column(Integer)
+    weight = Column(Integer)
+    base_experience = Column(Integer)
+    is_default = Column(Boolean)
